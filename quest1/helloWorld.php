115d7041a1792795d5c55279dbfb3852f08ec9f1
tanaka katsuya <gu0520kt@gmail.com>
2021-01-11 13:25:34 +0900

    テスト問題更新

--- quest1/helloWorld.php
+++ quest1/helloWorld.php
@@ -1,11 +1,17 @@
 <?php
- 
+
 //出力値
 //7
 //2
 //6
 
-$inputList = [7, 2, /*/ /*/];
-foreach ($inputList as $input) {
-	echo $input . "\n";
+function helloWorld()
+{
+  $output = "";
+  $inputList = [7, 2, 6];
+  foreach ($inputList as $input) {
+    $output .= $input . "\n";
+  }
+  return $output;
 }
+echo helloWorld();
